e70ba35c5844a7e24d555906eea7b6c339a11501
Guideon Katumbi <guideon.katumbi@zatechub.com>
2020-05-01 21:34:09 +0200

    Symptom table fix

--- resources/views/symptoms/index.blade.php
+++ resources/views/symptoms/index.blade.php
@@ -22,20 +22,15 @@
                 <table id="survey" class="table table-striped table-bordered" cellspacing="0" width="100%">
                     <thead>
                         <tr>
+                            <th>Actions</th>
                             <th>Name</th>
                             <th>Instant Help</th>
                             <th>ResPrio</th>
-                            {{-- <th>Fear</th>
-                            <th>Anger</th>
-                            <th>Sadness</th> --}}
                             <th>Belief</th>
-                            <th>Book url</th>
-                            <th>Book img </th>
+                            <th>Book </th>
                             <th>Book description</th>
-                            <th>Recom Program url</th>
-                            <th>Recom Program image</th>
+                            <th>Recom Program</th>
                             <th>Recom Program description</th>
-                            <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -44,6 +39,14 @@
                         @endphp
                         @foreach ($areaOfLife->symptoms as $key => $symptom)
                             <tr>
+                                <td>
+                                    <a href="/area/{{$areaOfLife->id}}/symptom/{{$symptom->id}}/edit" style="color:green;"><i class="btn c-blue-500 ti-pencil-alt"></i></a>
+                                    <form action="{{ route('symptom.destroy', [$areaOfLife->id, $symptom->id]) }}" method="POST">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button class="btn" onclick="return confirm('!! Are you sure you want to delete this symtom and all its mappings? !! ')"><i class="c-red-500 ti-trash"></i></button>
+                                    </form>
+                                </td>
                                 <td>{{$symptom->name}}</td>
                                 <td>
                                     <span>
@@ -55,12 +58,10 @@
                                     <button class="btn btn-link" onclick="showMore({{$key + 100}})">Read more...</button>
                                 </td>
                                 <td>{{$symptom->res_prio}}</td>
-                                {{-- <td>{{$symptom->fear}}</td>
-                                <td>{{$symptom->anger}}</td>
-                                <td>{{$symptom->sadness}}</td> --}}
                                 <td>{{$symptom->belief}}</td>
-                                <td><a href="{{$symptom->recom_book_url}}" target="_blank">{{$symptom->recom_book_url}}</a></td>
-                                <td><a href="{{$symptom->recom_book_image}}" target="_blank"><img src="{{$symptom->recom_book_image}}" height="150" width="100" /></a></td>
+                                <td>
+                                    <a href="{{$symptom->recom_book_url}}" target="_blank"><img src="{{$symptom->recom_book_image}}" height="150" width="100" /></a>
+                                </td>
                                 <td>
                                     <span>
                                         {{substr($symptom->recom_book_description, 0, 50)}}
@@ -69,7 +70,7 @@
                                         {{substr($symptom->recom_book_description, 50)}}
                                     </span>
                                     <button class="btn btn-link" onclick="showMore({{$key + 200}})">Read more...</button>
-                                <td><a href="{{$symptom->recom_program_url}}">{{$symptom->recom_program_url}}</a></td>
+                                </td>
                                 <td>
                                     @php
                                         $ext = strtolower(pathinfo($symptom->recom_program_image, PATHINFO_EXTENSION));
@@ -77,7 +78,7 @@
                                     @if(in_array($ext, $supportImageExtensions))
                                         <a href="{{$symptom->recom_program_url}}" target="_blank"><img src="{{$symptom->recom_program_image}}" height="150" width="100" /></a>
                                     @else
-                                        <a href="{{$symptom->recom_program_image}}">{{$symptom->recom_program_image}}</a>
+                                        <a href="{{$symptom->recom_program_url}}" target="_blank"><img src="{{ asset('all/images/hapily-coach-image2.png')}}" alt=""  height="150" width="100"></a>
                                     @endif
                                 </td>
                                 <td>
@@ -88,13 +89,6 @@
                                         {{substr($symptom->recom_program_description, 50)}}
                                     </span>
                                     <button class="btn btn-link" onclick="showMore({{$key + 300}})">Read more...</button>
-                                <td>
-                                    <a href="/area/{{$areaOfLife->id}}/symptom/{{$symptom->id}}/edit" style="color:green;"><i class="btn c-blue-500 ti-pencil-alt"></i></a>
-                                    <form action="{{ route('symptom.destroy', [$areaOfLife->id, $symptom->id]) }}" method="POST">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button class="btn" onclick="return confirm('!! Are you sure you want to delete this symtom and all its mappings? !! ')"><i class="c-red-500 ti-trash"></i></button>
-                                    </form>
                                 </td>
                             </tr>
                         @endforeach
@@ -112,9 +106,9 @@
 <script>
     $(document).ready(function() {
         $('#survey').dataTable({
-            "scrollY":        "600px",
-            "scrollCollapse": true,
-            "scrollX": true
+            // "scrollY":        "600px",
+            // "scrollCollapse": true,
+            // "scrollX": true
         });
     });
 </script>
