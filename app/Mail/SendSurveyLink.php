ce81a1923a465445867f52faa4853c8397bc539d
Guideon Katumbi <guideon.katumbi@zatechub.com>
2020-05-07 00:43:42 +0200

    Email from removed/testing

--- app/Mail/SendSurveyLink.php
+++ app/Mail/SendSurveyLink.php
@@ -31,7 +31,8 @@ class SendSurveyLink extends Mailable
     public function build()
     {
         //Shmock code = =?utf-8?Q?=26=23x2618=3B?=   OR =?utf-8?Q?=E2=98=98=EF=B8=8F?=
-        return $this->from('hallo@hapily.de',  'Denis von hapily')
+        //return $this->from('hallo@hapily.de',  'Denis von hapily')
+        return $this->from('hallo@hapily.de',  'hapily')
                     ->subject('=?utf-8?Q?=E2=98=98=EF=B8=8F?= Dein Glücks-Bericht') //Added Emoji
                     ->view('surveys.email')
                     ->with('data', $this->data);
