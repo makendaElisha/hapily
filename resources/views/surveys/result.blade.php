3bbe63d5d875189370e41a4e54bd8c8451062658
Jdfkat <5977296+Jdfkat@users.noreply.github.com>
2020-05-07 14:15:03 +0200

    Gratis-teaching style update.
    Gratis-teaching style update.

--- resources/views/surveys/result.blade.php
+++ resources/views/surveys/result.blade.php
@@ -223,7 +223,7 @@
                               @php
                                   //Making Gratis-Coaching bold
                                   if(strpos($symptom->recom_program_description, 'Gratis-Coaching') !== false){
-                                    $programText = str_replace('Gratis-Coaching', '<b>Gratis-Coaching</b>', $symptom->recom_program_description);
+                                    $programText = strip_tags(str_replace('Gratis-Coaching', '<b>Gratis-Coaching</b>', $symptom->recom_program_description));
                                   } else {
                                     $programText = $symptom->recom_program_description;
                                   }
@@ -352,7 +352,7 @@
                                   @php
                                       //Making Gratis-Coaching bold
                                       if(strpos($symptom->recom_program_description, 'Gratis-Coaching') !== false){
-                                         $programText = str_replace('Gratis-Coaching', '<b>Gratis-Coaching</b>', $symptom->recom_program_description);
+                                         $programText = strip_tags(str_replace('Gratis-Coaching', '<b>Gratis-Coaching</b>', $symptom->recom_program_description));
                                       } else {
                                         $programText = $symptom->recom_program_description;
                                       }
