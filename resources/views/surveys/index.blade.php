6756f4ba7d338b0607c24756655cc78fefb1c843
Guideon Katumbi <guideon.katumbi@zatechub.com>
2020-05-10 20:08:24 +0200

    Data table fix

--- resources/views/surveys/index.blade.php
+++ resources/views/surveys/index.blade.php
@@ -2,14 +2,6 @@
 
 @section('content')
 
-<style>
-    table.dataTable thead .sorting { background: url("/all/datatable/sort_both.png") no-repeat center right; }
-    table.dataTable thead .sorting_asc { background: url("/all/datatable/sort_asc.png") no-repeat center right; }
-    table.dataTable thead .sorting_desc { background: url("/all/datatable/sort_desc.png") no-repeat center right; }
-    table.dataTable thead .sorting_asc_disabled { background: url("/all/datatable/sort_asc_disabled.png") no-repeat center right; }
-    table.dataTable thead .sorting_desc_disabled { background: url("/all/datatable/sort_desc_disabled.png") no-repeat center right; }
-</style>
-
 <div class="container-fluid">
     <h4 class="c-grey-900 mT-10 mB-20">Data Tables</h4>
     <a href="/survey/submit" type="button" class="btn btn-primary c-white mB-10" style="background-color:purple;">Simulate Survey :)</a>
@@ -50,8 +42,18 @@
 
 <script>
     $(document).ready(function() {
-        $('#survey').dataTable({
+        $('#dataTable').DataTable({
 
+            columns: [
+                {
+                    data: 'submit_date',
+                    type: 'num',
+                    render: {
+                        _: 'display',
+                        sort: 'timestamp'
+                    }
+                }
+            ]
         });
     });
 </script>
