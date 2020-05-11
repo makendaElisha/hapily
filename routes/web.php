bc457a85c71fda2175e0f4d51e15717acb3d895b
Guideon Katumbi <guideon.katumbi@zatechub.com>
2020-05-11 22:34:18 +0200

    Route clean up

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,5 @@
 <?php
 
-use App\Entities\Score;
-use App\Entities\Answer;
-use App\Entities\Customer;
-use App\Mail\SendSurveyLink;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Cache;
-use Omniphx\Forrest\Providers\Laravel\Facades\Forrest;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,7 +10,6 @@ use Omniphx\Forrest\Providers\Laravel\Facades\Forrest;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 
 
 Route::get('/', function () {
@@ -43,9 +34,6 @@ Route::middleware(['auth'])->group(function () {
     Route::put('/area-of-life/{areaOfLife}', 'AreaOfLifeController@update')->name('area.update');
     Route::get('/area-of-life/{areaOfLife}', 'AreaOfLifeController@edit')->name('area.show');
 
-    //Symptom
-    // Route::resource('/symptom', 'SymptomController'); //To go
-
     //User
     Route::resource('user', 'UserController'); //To be fixed
 
@@ -61,167 +49,8 @@ Route::middleware(['auth'])->group(function () {
     //Survey
     Route::get('/survey', 'SurveyController@index')->name('survey.index');
     Route::get('/survey/submit', 'SurveyController@receiveSurvey')->name('survey.simulate');
-    //Route::post('/survey/result/{token}', 'SurveyController@userResult')->name('survey.result');
-    // Route::get('/survey/result/{token}', 'SurveyController@userResult')->name('survey.result');
-
     Route::view('/profile', 'profile.index');
 
 });
 
-
-Route::get('/leadata', function(){
-
-    $scoreCustomer      = Score::where('customer_id', 1)->first();
-    $answersCustomer    = Answer::where('customer_id', 1)->get();
-    $customerData       = Customer::where('id', 1)->first();
-
-    $symptomsCarrer         = '';
-    $symptomsLove           = '';
-    $symptomsSexuality      = '';
-    $symptomsBodayHealth    = '';
-    $symptomsFriendship     = '';
-    $symptomsFamily         = '';
-    $symptomsSpirituality   = '';
-    $symptomDefault         = '';
-
-    foreach($answersCustomer as $answer) {
-        switch ($answer->reference) {
-            case 'symptoms_beruf_und_karriere_user':
-                $symptomsCarrer .= $answer->name . '\n';
-                break;
-            case 'symptoms_partnerschaft_user':
-                $symptomsLove .= $answer->name . '\n';
-                break;
-            case 'symptoms_sexualitaet_user':
-                $symptomsSexuality .= $answer->name . '\n';
-                break;
-            case 'symptoms_koerper_und_gesundheit_user':
-                $symptomsBodayHealth .= $answer->name . '\n';
-                break;
-            case 'symptoms_freundschaften_user':
-                $symptomsFriendship .= $answer->name . '\n';
-                break;
-            case 'symptoms_familie_user':
-                $symptomsFamily .= $answer->name . '\n';
-                break;
-            case 'symptoms_spiritualitaet_user':
-                $symptomsSpirituality .= $answer->name . '\n';
-                break;
-            default:
-                $symptomDefault = 'No symptom found';
-        }
-    }
-
-    //'Gender'                        => $customerData->gender == 'Männlich' ? 'Male' : 'Female',
-    if($customerData->gender == 'Männlich'){
-        $gender = 'Male';
-    }elseif($customerData->gender == 'Weiblich'){
-        $gender = 'Female';
-    }else{
-        $gender = 'Other';
-    }
-
-    $leadSalesForce = [
-        'FirstName'                     => $customerData->prename,
-        'email'                         => $customerData->email,
-        'Gender'                        => $gender, 
-        'MobilePhone'                   => $customerData->phone_number,
-        'Overall_Happiness_Score'       => $scoreCustomer->total_areas,
-        'Happiness_Score_Career'        => $scoreCustomer->beruf_und_karriere,
-        'Happiness_Score_Love'          => $scoreCustomer->partnerschaft,
-        'Happiness_Score_Sexuality'     => $scoreCustomer->sexualitaet,
-        'Happiness_Score_Body_Health'   => $scoreCustomer->koerper_und_gesundheit,
-        'Happiness_Score_Friendship'    => $scoreCustomer->freundschaften,
-        'Happiness_Score_Family'        => $scoreCustomer->familie,
-        'Happiness_Score_Spirituality'  => $scoreCustomer->spiritualitaet,
-        'Symptoms_Career'               => $symptomsCarrer,
-        'Symptoms_Love'                 => $symptomsLove,
-        'Symptoms_Sexuality'            => $symptomsSexuality,
-        'Symptoms_Body_Health'          => $symptomsBodayHealth,
-        'Symptoms_Friendship'           => $symptomsFriendship,
-        'Symptoms_Family'               => $symptomsFamily,
-        'Symptoms_Spirituality'         => $symptomsSpirituality,
-        'Time_Invest_Willingness'       => $customerData->time_invest_willingness,
-        'Money_Invest_Willingness'      => $customerData->money_invest_willingness,
-        'Newsletter_Opt_in'             => $customerData->newsletter_opt_in,
-        'Call_Opt_in'                   => $customerData->call_opt_in,
-        'Survey_Result_URL'             => url($customerData->survey_url),
-    ];
-
-    return $leadSalesForce;
-
-});
-
-// Route::get('/home', 'HomeController@index')->name('home');
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/authenticate', function() {
-    // return Forrest::getmytoken();
-    dd(Forrest::getmytoken());
-});
-
-Route::get('/curl-authenticate', function() {
-    dd(Forrest::curlGetToken());
-});
-
-Route::get('/tokensurvey', 'SurveyController@curlGetTokenSalesForce');
-
-Route::get('/createlead', 'SurveyController@createLead');
-
-Route::get('/callback/salesforce', function() {
-    Forrest::callback();
-});
-
-Route::get('/sessions', function(){
-
-    $data = [
-        'session' => session()->all(),
-        //'forrestVersion' => Forrest::versions(),
-        'forrestToken' => Cache::get('forrest_token'),
-    ];
-
-    dd($data);
-});
-
-
 Route::post('/callback/survey', 'SurveyController@surveyHook')->name('survey.surveyHook');
-
-
-Route::get('/table', function () {
-    return view('table');
-});
-
-Route::get('/form', function () {
-    return view('form');
-});
-
-
-Route::get('/symptoms', function () {
-    return view('symptoms');
-});
-
-Route::get('/surveys', function() {
-    return view('surveys');
-});
-
-
-// Route::get('/login', function() {
-//     return view('login');
-// });
-
-Route::get('/survey-email', function(){
-
-    $data = [
-        'name'          => 'Test User',
-        'surveyLink'    => 'https://hapily.de'
-    ];
-
-    Mail::to('python.gralf@gmail.com')
-        ->send(new SendSurveyLink($data));
-
-    //return view('surveys.email');
-});
-
