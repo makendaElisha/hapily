8022d9117e43c5724afe427b63f054b4bb1a5b13
root <root@ip-172-31-46-71.eu-central-1.compute.internal>
2020-08-15 16:17:03 +0200

    Update for old newsletter subscribers

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -466,11 +466,11 @@ class SurveyController extends Controller
         //Customers subscribed but didn't opt in for call in
         //$customers = Customer::where('id', '<', 293)->get(); //ID 293 Julia was already added to newsletter list
         //1st of june to 14 august 2020
-        $customers = Customer::whereBetween("submit_date", array(Carbon::now()->subDays(74), Carbon::now()->subDays(1)))->get();
+        $customers = Customer::whereBetween("submit_date", array(Carbon::now()->subDays(44), Carbon::now()->subDays(1)))->get();
 
         foreach ($customers as $customer){
-            if ($customer->newsletter_opt_in === 1 && $customer->call_opt_in === 0) {
-                (new ContactSubscriptionService)->handleNonCallOptinUsers($customer);
+            if ($customer->newsletter_opt_in === 1) {
+                (new ContactSubscriptionService)->handleTestSubscription($customer);
                 echo 'Customer is ' . $customer->prename . '<br />'; //works
             }
         }
