c6464576ea39e0866f5f326d85dece5e8d40c03d
Guideon Katumbi <guideon.katumbi@zatechub.com>
2020-08-15 15:29:31 +0200

    Mergin with teachable api functionalities

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -293,11 +293,11 @@ class SurveyController extends Controller
      */
     public function surveyHook(Request $request)
     {
-        // try {
-        //     Storage::prepend('survey_submission.txt', '----' . json_encode($request->all()) . '----');
-        // } catch (\Exception $e) {
-        //     logger()->error('Error appending new survey to file: ' . $e->getMessage());
-        // }
+        try {
+            Storage::prepend('survey_submission.txt', '----' . json_encode($request->all()) . '----');
+        } catch (\Exception $e) {
+            logger()->error('Error appending new survey to file: ' . $e->getMessage());
+        }
 
         $dataArray = json_decode(json_encode($request->all()), true);
 
@@ -326,6 +326,11 @@ class SurveyController extends Controller
             (new ContactSubscriptionService)->handleNewsletterSubscription($customer);
         } else {
             (new ContactSubscriptionService)->handleNonSubscribersAutomation($customer);
+        }
+
+        //If customer subscribed but didn't optin for the call
+        if ($customer->newsletter_opt_in == 1 && $customer->call_opt_in == 0) {
+            (new ContactSubscriptionService)->handleNonCallOptinUsersAutomation($customer);
         }
 
         //Send a slack notification
@@ -444,18 +449,32 @@ class SurveyController extends Controller
         //     }
         // }
 
-        $user = [
-            'prename' => 'Denis Test',
-            'email' => 'denismartin.coaching@gmail.com'
-        ];
 
-        $userObject = (object) $user;
-        (new ContactSubscriptionService)->handleNewsletterSubscription($userObject);
+        // $user = [
+        //     'prename' => 'Denis Martin',
+        //     'email' => 'denismartin.coaching@gmail.com'
+        // ];
 
-        echo 'User ' . $userObject->prename . ' has been added to the list.<br />'; //works
+        // $userObject = (object) $user;
+        // (new ContactSubscriptionService)->handleTestSubscription($userObject);
+
+        // echo 'User ' . $userObject->prename . ' has been added to the list.<br />'; //works
+
+        // //return "server up and running...";
 
 
-        //return "server up and running...";
+        //Customers subscribed but didn't opt in for call in
+        //$customers = Customer::where('id', '<', 293)->get(); //ID 293 Julia was already added to newsletter list
+        //1st of june to 14 august 2020
+        $customers = Customer::whereBetween("submit_date", array(Carbon::now()->subDays(74), Carbon::now()->subDays(1)))->get();
+
+        foreach ($customers as $customer){
+            if ($customer->newsletter_opt_in === 1 && $customer->call_opt_in === 0) {
+                (new ContactSubscriptionService)->handleNonCallOptinUsers($customer);
+                echo 'Customer is ' . $customer->prename . '<br />'; //works
+            }
+        }
+
     }
     
 }
--- app/Services/Mailjet/ContactSubscriptionService.php
+++ app/Services/Mailjet/ContactSubscriptionService.php
@@ -20,8 +20,9 @@ class ContactSubscriptionService
     protected const NEWSLETTER_USER_LIST = 25335;
     protected const WEBINAR_PAID_USER_LIST = 28521;
     protected const AUTOMATION_NON_SUBSCRIBER_USER_LIST = 29767; //29766 for non subscribers transational list
-    protected const WEBSITE_SUBSCRIBER_LIST = 33512;
-    protected const NON_CALL_USERS_LIST = 33405;
+    protected const NON_CALL_USERS_LIST_TRANSACTIONAL = 33405;
+    protected const NON_CALL_USERS_LIST_AUTOMATION = 33840;
+    protected const TEST_LIST = 33765;
 
     /**
      * @param mixed $handleAutomationSubscription
@@ -31,15 +32,27 @@ class ContactSubscriptionService
     public function handleAutomationSubscription($customer)
     {
         // Body request
+        /*
         $body = [
             'Name' => $customer->prename,
             'Properties' => ['vorname' => $customer->prename], //use Vorname to match the list vorname
             'Action' => "addnoforce",
             'Email' => $customer->email,
+        ];*/
+
+        $body = [
+            'Action' => "addnoforce",
+            'Contacts' => [
+              [
+                'Email' => $customer->email,
+                'Name' => $customer->prename,
+                'Properties' => ['vorname' => $customer->prename],
+              ]
+            ]
         ];
 
         try {
-            $response = Mailjet::post(Resources::$ContactslistManagecontact, ['id' => self::AUTOMATION_USER_LIST, 'body' => $body]);
+            $response = Mailjet::post(Resources::$ContactslistManagemanycontacts, ['id' => self::AUTOMATION_USER_LIST, 'body' => $body]);
         } catch (\Exception $e) {
             logger()->error('Error adding subscriber to list: ' . $e->getMessage(), $customer->prename);
         }
@@ -54,15 +67,28 @@ class ContactSubscriptionService
     public function handleNewsletterSubscription($customer)
     {
         /* Body request */
+        /*
         $body = [
             'Name' => $customer->prename,
             'Properties' => ['vorname' => $customer->prename], //use Vorname to match the list vorname
             'Action' => "addnoforce",
             'Email' => $customer->email,
         ];
+        */
+
+        $body = [
+            'Action' => "addnoforce",
+            'Contacts' => [
+              [
+                'Email' => $customer->email,
+                'Name' => $customer->prename,
+                'Properties' => ['vorname' => $customer->prename],
+              ]
+            ]
+        ];
 
         try {
-            $response = Mailjet::post(Resources::$ContactslistManagecontact, ['id' => self::NEWSLETTER_USER_LIST, 'body' => $body]);
+            $response = Mailjet::post(Resources::$ContactslistManagemanycontacts, ['id' => self::NEWSLETTER_USER_LIST, 'body' => $body]);
         } catch (\Exception $e) {
             logger()->error('error adding subscriber to list: ' . $e->getMessage(), $customer->prename);
         }
@@ -77,15 +103,27 @@ class ContactSubscriptionService
     public function handleNonSubscribersAutomation($customer)
     {
         // Body request
+        /*
         $body = [
             'Name' => $customer->prename,
             'Properties' => ['vorname' => $customer->prename], //use Vorname to match the list vorname
             'Action' => "addnoforce",
             'Email' => $customer->email,
+        ];*/
+
+        $body = [
+            'Action' => "addnoforce",
+            'Contacts' => [
+              [
+                'Email' => $customer->email,
+                'Name' => $customer->prename,
+                'Properties' => ['vorname' => $customer->prename],
+              ]
+            ]
         ];
 
         try {
-            $response = Mailjet::post(Resources::$ContactslistManagecontact, ['id' => self::AUTOMATION_NON_SUBSCRIBER_USER_LIST, 'body' => $body]);
+            $response = Mailjet::post(Resources::$ContactslistManagemanycontacts, ['id' => self::AUTOMATION_NON_SUBSCRIBER_USER_LIST, 'body' => $body]);
         } catch (\Exception $e) {
             logger()->error('Error adding subscriber to list: ' . $e->getMessage(), $customer->prename);
         }
@@ -100,15 +138,27 @@ class ContactSubscriptionService
     public function handlePaidUserSubscription($payment)
     {
         /* Body request */
+        /*
         $body = [
             'Name' => $payment->buyer_first_name,
             'Properties' => ['vorname' => $payment->buyer_first_name], //need to add webinar session properties but error
             'Action' => "addnoforce",
             'Email' => $payment->buyer_email,
+        ];*/
+
+		$body = [
+            'Action' => "addnoforce",
+            'Contacts' => [
+              [
+                'Email' => $payment->buyer_email,
+                'Name' => $payment->buyer_first_name,
+                'Properties' => ['vorname' => $payment->buyer_first_name,],
+              ]
+            ]
         ];
 
         try {
-            $response = Mailjet::post(Resources::$ContactslistManagecontact, ['id' => self::WEBINAR_PAID_USER_LIST, 'body' => $body]);
+            $response = Mailjet::post(Resources::$ContactslistManagemanycontacts, ['id' => self::WEBINAR_PAID_USER_LIST, 'body' => $body]);
         } catch (\Exception $e) {
             // logger()->error('error adding subscriber to list: ' . $e->getMessage(), $payment->buyer_first_name);
             Storage::put('error.txt', $e);
@@ -116,47 +166,89 @@ class ContactSubscriptionService
     }
 
     /**
-     * @param mixed $handleWebsiteOptinMonster Subscription
-     * @param mixed $request
-     */
-
-    public function handleWebsiteSubscription($request)
-    {
-        /* Body request */
-        $body = [
-            'Name' => $request->firstName . " " . $request->lastName,
-            'Properties' => ['vorname' => $request->firstName],
-            'Action' => "addnoforce",
-            'Email' => $request->email,
-        ];
-
-        try {
-            $response = Mailjet::post(Resources::$ContactslistManagecontact, ['id' => self::WEBSITE_SUBSCRIBER_LIST, 'body' => $body]);
-        } catch (\Exception $e) {
-            logger()->error('Error adding subscriber to list: ' . $e->getMessage(), $request->firstName);
-        }
-    }
-
-    /**
      * @param mixed $handleNonCallOptinUsers
      * @param mixed $payment
-    */
+     */
 
     public function handleNonCallOptinUsers($customer)
     {
         // Body request
+        // $body = [
+        //     'Name' => $customer->prename,
+        //     'Properties' => ['vorname' => $customer->prename], //use Vorname to match the list vorname
+        //     'Action' => "addnoforce",
+        //     'Email' => $customer->email,
+        // ];
+
         $body = [
-            'Name' => $customer->prename,
-            'Properties' => ['vorname' => $customer->prename], //use Vorname to match the list vorname
             'Action' => "addnoforce",
-            'Email' => $customer->email,
+            'Contacts' => [
+              [
+                'Email' => $customer->email,
+                'Name' => $customer->prename,
+                'Properties' => ['vorname' => $customer->prename],
+              ]
+            ]
         ];
 
         try {
-            $response = Mailjet::post(Resources::$ContactslistManagecontact, ['id' => self::NON_CALL_USERS_LIST, 'body' => $body]);
+            $response = Mailjet::post(Resources::$ContactslistManagemanycontacts, ['id' => self::NON_CALL_USERS_LIST_TRANSACTIONAL, 'body' => $body]);
         } catch (\Exception $e) {
             logger()->error('Error adding subscriber to list: ' . $e->getMessage(), $customer->prename);
         }
     }
+
+
+    public function handleNonCallOptinUsersAutomation($customer)
+    {
+        // Body request
+        // $body = [
+        //     'Name' => $customer->prename,
+        //     'Properties' => ['vorname' => $customer->prename], //use Vorname to match the list vorname
+        //     'Action' => "addnoforce",
+        //     'Email' => $customer->email,
+        // ];
+
+        $body = [
+            'Action' => "addnoforce",
+            'Contacts' => [
+              [
+                'Email' => $customer->email,
+                'Name' => $customer->prename,
+                'Properties' => ['vorname' => $customer->prename],
+              ]
+            ]
+        ];
+
+        try {
+            $response = Mailjet::post(Resources::$ContactslistManagemanycontacts, ['id' => self::NON_CALL_USERS_LIST_AUTOMATION, 'body' => $body]);
+        } catch (\Exception $e) {
+            logger()->error('Error adding subscriber to list: ' . $e->getMessage(), $customer->prename);
+        }
+    }
+    
+    
+    
+    //Test List
+    public function handleTestSubscription($customer)
+    {
+        // Body request
+		$body = [
+		  'Action' => "addnoforce",
+		  'Contacts' => [
+			[
+			  'Email' => $customer->email,
+			  'Name' => $customer->prename,
+			  'Properties' => ['vorname' => $customer->prename],
+			]
+		  ]
+		];
+
+        try {
+            $response = Mailjet::post(Resources::$ContactslistManagemanycontacts, ['id' => self::TEST_LIST, 'body' => $body]);
+        } catch (\Exception $e) {
+            logger()->error('Error adding subscriber to list: ' . $e->getMessage(), $customer->prename);
+        }
+	}
 
 }
